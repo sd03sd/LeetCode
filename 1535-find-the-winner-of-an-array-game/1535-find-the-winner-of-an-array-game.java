e2c6df62d2a1bcd8c20d6cb368ea0fd58befdf0e
MOHD SAHIL <128277597+sd03sd@users.noreply.github.com>
2023-11-05 10:40:14 +0530

    Time: 0 ms (100.00%), Space: 56.3 MB (74.64%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int getWinner(int[] arr, int k) {
+        int max = arr[0];
+        int count = 0;
+
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                count = 1;
+            }else{
+                count++;
+            }
+            if(count == k){
+                return max;
+            }
+        }
+        return max;
+    }
+}
